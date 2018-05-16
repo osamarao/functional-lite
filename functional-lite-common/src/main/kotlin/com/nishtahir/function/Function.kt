68e34ad0fa2a3fe16a8aef9b5120d4028bd57ae9
osamarao <osama.rao@willowtreeapps.com>
2018-05-16 14:58:56 -0400

    compose function

--- functional-lite-common/src/main/kotlin/com/nishtahir/function/Function.kt
+++ functional-lite-common/src/main/kotlin/com/nishtahir/function/Function.kt
@@ -1,3 +1,5 @@
 package com.nishtahir.function
 
 inline fun <A, B, C> ((A) -> B).andThen(crossinline transform: (B) -> C): (A) -> C = { a -> transform(this(a)) }
+
+inline fun <A, B, C> ((A) -> B).compose(crossinline func1: (B) -> C) = { i: A -> func1(this(i)) }
